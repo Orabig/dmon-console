b3488fe4010b12a1dd7db346d2dff388a4a4f89c
Orabig <benoit.chauvet@gmail.com>
2017-08-22 16:49:56 +0200

    unregister service button

--- src/app/send-command.service.ts
+++ src/app/send-command.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Headers, Http, Response } from '@angular/http';
 
 import { Host } from './host';
+import { Service } from './service';
 
 // Le service qui envoie des commandes au serveur
 
@@ -33,6 +34,19 @@ export class SendCommandService {
 				'host-id': host.name,
 				cmd: 'RUN',
 				args:  {cmdline: host.cmdline}
+			}
+		).subscribe(response=>console.log(response));
+  }
+	
+  sendUnregister(host: Host, service: Service): void {
+		this.postJson(this.baseApiURL + "send-order.php",
+			{
+				t: 'CMD',
+				// 'id': cmdId,
+				'client-id': host.client, 
+				'host-id': host.name,
+				cmd: 'UNREGISTER',
+				args:  {serviceId: service.name}
 			}
 		).subscribe(response=>console.log(response));
   }
--- src/app/service-state.component.ts
+++ src/app/service-state.component.ts
@@ -1,11 +1,21 @@
 import { Component, Input } from '@angular/core';
 
+import { SendCommandService } from './send-command.service';
+
+import { Host } from './host';
 import { Service } from './service';
+
 @Component({
   selector: 'service-state',
   templateUrl: './service-state.component.html',
   styleUrls: ['./service-state.component.css']
 })
 export class ServiceStateComponent {
+	constructor(private sendCommandService: SendCommandService) { }
+	@Input() host: Host;
 	@Input() service: Service;
+	
+	unRegister():void {
+		this.sendCommandService.sendUnregister(this.host, this.service);
+	}
 }
