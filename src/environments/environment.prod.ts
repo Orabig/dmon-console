181429e50359fa97b79cfeb7057235d4f85d0fd9
Orabig <benoit.chauvet@gmail.com>
2017-08-27 21:58:36 +0200

    Preparation env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,9 @@
 export const environment = {
   production: true,
-  centrifugoServerUrl: 'http://centrifugo.crocoware.com:8000/connection',
-  centrifugoAuthEndpoint: "http://centrifugo.crocoware.com:9191/api/auth.php",
-  dmonApiRoot: 'http://centrifugo.crocoware.com:9191/api/'
+  centrifugoServerUrl: 'https://centrifugo.crocoware.com/connection',
+  centrifugoAuthEndpoint: "https://dmon.crocoware.com/api/auth.php",
+  dmonApiRoot: 'https://dmon.crocoware.com/api/'
+//  centrifugoServerUrl: 'http://centrifugo.crocoware.com:8000/connection',
+//  centrifugoAuthEndpoint: "http://centrifugo.crocoware.com:9191/api/auth.php",
+//  dmonApiRoot: 'http://centrifugo.crocoware.com:9191/api/'
 };
