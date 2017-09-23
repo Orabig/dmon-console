be5d5bf0b3a2901788c389257ceda0bf8b4e4f7b
Benoit Chauvet <benoit.chauvet@gmail.com>
2017-09-23 20:03:43 +0200

    move extractPluginList to rules.ts

--- src/app/_helpers/rules.ts
+++ src/app/_helpers/rules.ts
@@ -1,7 +1,37 @@
 import { Agent } from '../_models/objects';
 import { Command } from '../_models/templates';
 
-// Business rules
+// ----------------- Discovery rules
+
+// Decodes output of "!check --list-plugin" and transforms to a plugin list
+export function extractPluginList(stdout: string[]): any[] {
+  var pluginRE = /^PLUGIN: (\S+)DESCRIPTION: *(.*?) *$/;
+  return stdout.join("").split(/-{5,}/) // chaque plugin est sur une seule ligne (et séparé par des lignes)
+				.sort()
+				.map( line => {
+					var match = pluginRE.exec(line); 
+					if (!match) return null;
+					var plugin = match[1];
+					var description = match[2].replace(/ +/g," ");
+					var name = description.replace(/^Check +(an? +)?/,"").replace(/ *(\.|\(|through|locally|in SNMP).*/,"");
+					var local = !!description.match(/\blocal(ly)?\b/);
+					var protocol = null;
+					if (description.match( /\bSNMP\b/i)) protocol='SNMP';
+					if (description.match( /\bSSH\b/i )) protocol='SSH';
+					if (description.match( /\bTCP|SMTP\b/i )) protocol='TCP';
+					if (description.match( /\bUDP\b/i )) protocol='UDP';
+					if (description.match( /\bAPI\b/i )) protocol='API';
+					if (description.match( /\bHTTP|Webpage\b/i )) protocol='HTTP';
+					if (description.match( /\bJMX\b/i )) protocol='JMX';
+					if (description.match( /\bws-management|WinRM|wsman\b/i )) protocol='WinRM';
+					if (description.match( /\bcan use SSH\b/i )) {
+						local = true; // There will be 2 families in that case (one local, and one with SSH)
+						protocol='SSH';
+					}
+					if (protocol==null) local=true;
+					return { name:name,	plugin:plugin, local:local, protocol:protocol, description:description, families:null};
+				}).filter( plugin => plugin != null );
+}
 
 // Tells if a given plugin's variable is dedicated to SSH
 // variable is {name:..., description:...}
--- src/app/page-plugin-discovery/page-plugin-discovery.component.ts
+++ src/app/page-plugin-discovery/page-plugin-discovery.component.ts
@@ -11,7 +11,13 @@ import { ObjectsDataService, TemplatesDataService, CentrifugeService, HostServic
 import { environment } from '../../environments/environment';
 
 import { generateUUID } from '../_helpers/utils';
-import { isSSHVariable, extractDefaultFromVariable, extractValidationFromVariable, isAdvancedVariable, extractCardinalityFromVariable, isMandatoryVariable } from '../_helpers/rules';
+import { extractPluginList, 
+		 isSSHVariable,
+		 extractDefaultFromVariable,
+		 extractValidationFromVariable,
+		 isAdvancedVariable,
+		 extractCardinalityFromVariable,
+		 isMandatoryVariable } from '../_helpers/rules';
 
 @Component({
   selector: 'app-page-plugin-discovery',
@@ -120,33 +126,8 @@ export class PagePluginDiscoveryComponent implements OnInit, OnDestroy {
   processPluginList(stdout: string[]) {
 	  // Display start of step3
 	  this.outputStep2 = stdout.slice(2,13).join("\n") + "\n(......)";
-	  // PROCESS
-	  var pluginRE = /^PLUGIN: (\S+)DESCRIPTION: *(.*?) *$/;
-	  this.plugins = stdout.join("").split(/-{5,}/) // chaque plugin est sur une seule ligne
-					.sort()
-					.map( line => {
-						var match = pluginRE.exec(line); 
-						if (!match) return null;
-						var plugin = match[1];
-						var description = match[2].replace(/ +/g," ");
-						var name = description.replace(/^Check +(an? +)?/,"").replace(/ *(\.|\(|through|locally|in SNMP).*/,"");
-						var local = !!description.match(/\blocal(ly)?\b/);
-						var protocol = null;
-						if (description.match( /\bSNMP\b/i)) protocol='SNMP';
-						if (description.match( /\bSSH\b/i )) protocol='SSH';
-						if (description.match( /\bTCP|SMTP\b/i )) protocol='TCP';
-						if (description.match( /\bUDP\b/i )) protocol='UDP';
-						if (description.match( /\bAPI\b/i )) protocol='API';
-						if (description.match( /\bHTTP|Webpage\b/i )) protocol='HTTP';
-						if (description.match( /\bJMX\b/i )) protocol='JMX';
-						if (description.match( /\bws-management|WinRM|wsman\b/i )) protocol='WinRM';
-						if (description.match( /\bcan use SSH\b/i )) {
-							local = true; // There will be 2 families in that case (one local, and one with SSH)
-							protocol='SSH';
-						}
-						if (protocol==null) local=true;
-						return { name:name,	plugin:plugin, local:local, protocol:protocol, description:description, families:null};
-					}).filter( plugin => plugin != null );
+	  // PROCESS the output of --list-plugin
+	  this.plugins = extractPluginList(stdout);
   }
   
   // USER clicked on a Plugin
